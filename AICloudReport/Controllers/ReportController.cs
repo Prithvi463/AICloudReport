0f83d640675ca52a5ea90baf1863ef338f11b4db
Prithvi463 <prithvirajiv@outlook.com>
2020-11-08 21:30:58 +0530

    Add project files.

---
+++ AICloudReport/Controllers/ReportController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AICloudReport.Services;
+using jsreport.AspNetCore;
+using jsreport.Types;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AICloudReport.Controllers
+{
+	public class ReportController : Controller
+	{
+		public readonly IViewRenderService _viewRenderService;
+
+		public IJsReportMVCService JsReportMVCService { get; }
+
+		public ReportController(IViewRenderService viewRenderService, IJsReportMVCService jsReportMVCService)
+		{
+			_viewRenderService = viewRenderService;
+			JsReportMVCService = jsReportMVCService;
+		}
+
+		[MiddlewareFilter(typeof(JsReportPipeline))]
+		public IActionResult SendQuote()
+		{
+			HttpContext.JsReportFeature()
+				.Recipe(Recipe.PhantomPdf)
+				.Configure((r) => r.Template.Phantom = new Phantom { Format = PhantomFormat.A4, Margin = "0cm" });
+			return View();
+		}
+	}
+}
---
+++ AICloudReport/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AICloudReport.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
